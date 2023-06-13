d78794f349d6d6c42bea0b060ef06d0b70979d4f
Nikki Lou <nalou4@gmail.com>
2023-06-13 03:06:25 -0700

    size adjusstments to about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,6 @@ const About = () => {
             <section>
                 <div className='row'>
                     <div className='col'>
-                        
                         <h1 className='intro'>Hi! I'm Nikki.</h1>
                         <p className='intro'>Software Engineer /<br></br>Web Developer</p>
 
@@ -17,13 +16,13 @@ const About = () => {
                             <div className="icon-row">
                                 <div className="icon box transition-fade">
                                     <a href="https://www.linkedin.com/in/nikkilou/">
-                                        <i class="fa-brands fa-linkedin fa-lg"></i>
+                                        <i class="fa-brands fa-linkedin fa-2xl"></i>
                                     </a>
                                 </div>
 
                                 <div className="icon box">
                                     <a href="https://www.instagram.com/nikki._lou_/">
-                                        <i class="fa-brands fa-instagram fa-lg"></i>
+                                        <i class="fa-brands fa-instagram fa-2xl"></i>
                                     </a>
                                 </div>
                             </div>
@@ -31,85 +30,25 @@ const About = () => {
                             <div className="icon-row">
                                 <div className="icon box">
                                     <a href="https://github.com/nalou4">
-                                        <i class="fa-brands fa-github fa-lg"></i>
+                                        <i class="fa-brands fa-github fa-2xl"></i>
                                     </a>
                                 </div>
 
                                 <div className="icon box">
                                     <a href="https://discordapp.com/users/nikaloo">
-                                    <i class="fa-brands fa-discord fa-lg"></i>
+                                        <i class="fa-brands fa-discord fa-2xl"></i>
                                     </a>
                                 </div>
                             </div>
                         </div>
 
                         <a href="https://nikki-lou.netlify.app/">
-                            <button id="portfolio">View my portfolio.</button>
+                            <button id="portfolio">Meet the Creator.</button>
                         </a>
                     </div>
                 </div>
             </section>
 
-
-
-
-            {/* <div className="container">
-
-                <a href="https://nikki-lou.netlify.app/">
-                    <button>See my portfolio.</button>
-                </a>
-
-                <h1>Hi! I'm Nikki, Software Developer</h1>
-                <p>Learn more</p>
-
-                <div className="icons">
-                    <div className="icon box transition-fade">
-                        <a href="https://zesty-centaur-003438.netlify.app/">
-                            <i className="bi bi-apple"></i>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
-                                className="bi bi-apple" viewBox="0 0 16 16">
-                                <path
-                                    d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z" />
-                                <path
-                                    d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z" />
-                            </svg>
-                        </a>
-                    </div>
-
-                    <div className="icon box">
-                        <a href="">
-                            <i className="bi bi-ui-radios-grid"></i>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
-                                className="bi bi-ui-radios-grid" viewBox="0 0 16 16">
-                                <path
-                                    d="M3.5 15a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zm9-9a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zm0 9a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zM16 3.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0zm-9 9a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0zm5.5 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zm-9-11a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 2a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
-                            </svg>
-                        </a>
-                    </div>
-
-                    <div className="icon box">
-                        <a href="">
-                            <i className="bi bi-brush"></i>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
-                                className="bi bi-brush" viewBox="0 0 16 16">
-                                <path
-                                    d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.118 8.118 0 0 1-3.078.132 3.659 3.659 0 0 1-.562-.135 1.382 1.382 0 0 1-.466-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.394-.197.625-.453.867-.826.095-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.201-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.176-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04zM4.705 11.912a1.23 1.23 0 0 0-.419-.1c-.246-.013-.573.05-.879.479-.197.275-.355.532-.5.777l-.105.177c-.106.181-.213.362-.32.528a3.39 3.39 0 0 1-.76.861c.69.112 1.736.111 2.657-.12.559-.139.843-.569.993-1.06a3.122 3.122 0 0 0 .126-.75l-.793-.792zm1.44.026c.12-.04.277-.1.458-.183a5.068 5.068 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.59 1.927-5.566 4.66-7.302 6.792-.442.543-.795 1.243-1.042 1.826-.121.288-.214.54-.275.72v.001l.575.575zm-4.973 3.04.007-.005a.031.031 0 0 1-.007.004zm3.582-3.043.002.001h-.002z" />
-                            </svg>
-                        </a>
-                    </div>
-
-                    <div className="icon box">
-                        <a href="">
-                            <i className="bi bi-android"></i>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
-                                className="bi bi-android" viewBox="0 0 16 16">
-                                <path
-                                    d="M2.76 3.061a.5.5 0 0 1 .679.2l1.283 2.352A8.94 8.94 0 0 1 8 5a8.94 8.94 0 0 1 3.278.613l1.283-2.352a.5.5 0 1 1 .878.478l-1.252 2.295C14.475 7.266 16 9.477 16 12H0c0-2.523 1.525-4.734 3.813-5.966L2.56 3.74a.5.5 0 0 1 .2-.678ZM5 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2Zm6 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" />
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-            </div> */}
         </>
     )
 };
